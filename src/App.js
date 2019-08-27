ac42e6a4127001c0a4752a77fb3965f748f5d9d8
pgomesdev <pgomesdev@gmail.com>
2019-08-27 07:06:05 -0300

    feat: customized status bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 
 import Profile from './pages/Profile';
 
 // import { Container } from './styles';
 
 export default function App() {
-  return <Profile />;
+  return (
+    <>
+      <StatusBar backgroundColor="#402845" barStyle="light-content" />
+      <Profile />
+    </>
+  );
 }
