2ae32237ddff656dfce2c774f5e4d92a55c863d3
pgomesdev <pgomesdev@gmail.com>
2019-08-27 07:33:58 -0300

    feat: implemented navigation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
 
-import Profile from './pages/Profile';
-
-// import { Container } from './styles';
+import createRouter from './routes';
 
 export default function App() {
-  return (
-    <>
-      <StatusBar backgroundColor="#402845" barStyle="light-content" />
-      <Profile />
-    </>
-  );
+  const signed = true;
+  const Routes = createRouter(signed);
+
+  return <Routes />;
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
+
 import App from './App';
 
 export default function index() {
-  return <App />;
+  return (
+    <>
+      <StatusBar backgroundColor="#402845" barStyle="light-content" />
+      <App />
+    </>
+  );
 }
