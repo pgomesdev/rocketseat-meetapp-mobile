1a1ddffa26158bfc89338253097d59d8bf3ddacd
pgomesdev <pgomesdev@gmail.com>
2019-08-30 06:43:00 -0300

    feat: create user with api in the signup page

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { TouchableOpacity } from 'react-native';
+import { ActivityIndicator, Alert, TouchableOpacity } from 'react-native';
+
+import api from '../../services/api';
 
 import Background from '../../components/Background';
 import {
@@ -16,24 +18,69 @@ import {
 import logo from '../../assets/logo.png';
 
 export default function SignUp({ navigation }) {
+  const [fullname, setFullname] = useState('');
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  async function handleSubmit() {
+    setLoading(true);
+
+    try {
+      await api.post('/users', { fullname, username, email, password });
+
+      Alert.alert('Conta criada com sucesso!');
+
+      setLoading(false);
+
+      navigation.navigate('SignIn');
+    } catch (e) {
+      setLoading(false);
+
+      Alert.alert(
+        'Ocorreu um erro enquanto a conta era criada. Tente novamente.'
+      );
+    }
+  }
+
   return (
     <Background>
       <Container>
         <Image source={logo} />
         <Form>
-          <TextInput placeholder="Digite seu nome" autoCapitalize="words" />
+          <TextInput
+            placeholder="Digite seu nome"
+            autoCapitalize="words"
+            onChangeText={setFullname}
+            value={fullname}
+          />
+          <TextInput
+            placeholder="Digite seu usuário"
+            autoCapitalize="none"
+            onChangeText={setUsername}
+            value={username}
+          />
           <TextInput
             placeholder="Digite seu e-mail"
             keyboardType="email-address"
             autoCapitalize="none"
+            onChangeText={setEmail}
+            value={email}
           />
           <TextInput
             placeholder="Digite sua senha"
             autoCapitalize="none"
             secureTextEntry
+            onChangeText={setPassword}
+            value={password}
           />
-          <SubmitButton>
-            <SubmitButtonText>Criar conta</SubmitButtonText>
+          <SubmitButton onPress={handleSubmit}>
+            {loading ? (
+              <ActivityIndicator size="small" color="#fff" />
+            ) : (
+              <SubmitButtonText>Criar conta</SubmitButtonText>
+            )}
           </SubmitButton>
         </Form>
         <TouchableOpacity onPress={() => navigation.navigate('SignIn')}>
