4134d43a733947d3bfdaa81f6d484e70b2986008
pgomesdev <pgomesdev@gmail.com>
2019-08-30 06:26:02 -0300

    refactor: removed user state

--- src/store/modules/rootSagas.js
+++ src/store/modules/rootSagas.js
@@ -1,8 +1,7 @@
 import { all } from 'redux-saga/effects';
 
 import auth from './auth/sagas';
-import user from './user/sagas';
 
 export default function* rootSagas() {
-  return yield all([auth, user]);
+  return yield all([auth]);
 }
